ea681e59e4f6c2e11f7119873c1686c1c6776a9a
Leo <leotrim.dani@bynder.com>
2021-03-24 10:27:04 +0100

    Create leo.py

---
+++ leo.py
@@ -0,0 +1,19 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2577
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red117\green114\blue185;\red32\green32\blue32;\red153\green168\blue186;
+\red88\green118\blue71;}
+{\*\expandedcolortbl;;\csgenericrgb\c45882\c44706\c72549;\csgenericrgb\c12549\c12549\c12549;\csgenericrgb\c60000\c65882\c72941;
+\csgenericrgb\c34510\c46275\c27843;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf2 \cb3 print\cf4 (\cf5 'Baby name'\cf4 )\
+first = \cf2 input \cf4 (\cf5 "Name "\cf4 )\
+second = \cf2 input \cf4 (\cf5 "Surname "\cf4 )\
+\cf2 print\cf4 (\cf5 'Hello ' \cf4 + first + \cf5 ' ' \cf4 + second)\
+\cf2 print\cf4 (\cf5 'Please type your baby age'\cf4 )\
+first = \cf2 input\cf4 (\cf5 'months '\cf4 )\
+second = \cf2 input\cf4 (\cf5 'weeks '\cf4 )\
+\cf2 print\cf4 (\cf5 'Your baby is ' \cf4 + first + \cf5 ' months' \cf4 + \cf5 ' and ' \cf4 + second + \cf5 ' weeks' \cf4 )\
+\
+}
